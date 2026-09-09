401c59316c4163b19427df3e53b6ee74e43f8b06
zeidiniopdc-forex <zeidi.niopdc@gmail.com>
2026-09-09 14:56:01 +0330

    Add base path configuration to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/FitAI/",  // ⬅️ فقط این خط اضافه شد
+  base: "/FitAI/",  // ⬅️ این خط اضافه شد
   server: {
     host: "0.0.0.0",
     port: 3000,
